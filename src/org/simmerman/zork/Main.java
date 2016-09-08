83fde24679f3c0aeb664aebeca8d963b4ebd95e7
diamondcompass <toddjsimmerman@yahoo.com>
2016-09-08 09:22:55 -0500

    Refactored.  Renamed package structure.

--- src/org/simmerman/zork/Main.java
+++ src/org/simmerman/zork/Main.java
@@ -2,54 +2,20 @@ package org.simmerman.zork;
 
 //test
 
-import java.util.Scanner;
+import org.simmerman.zork.command.HotKey;
+import org.simmerman.zork.items.Items;
+import org.simmerman.zork.rooms.RoomOne;
 
 public class Main {
-
     public static void main(String[] args) {
-        Scanner keyboard = new Scanner(System.in);
+        Items.initializeItems();
+        System.out.println("Trapped With Precision");
+        HotKey.threeLineSkip();
+        System.out.println("Press Enter twice to begin game.");
+        HotKey.enterContinue();
+        HotKey.skipLines();
+        RoomOne.introScene();
+        System.out.println("You move on to room 2.");
 
-        String Start, Go, Drawer, End;
-
-        System.out.println("        Discord        ");
-        System.out.println("");
-        System.out.println("Would you like to begin the adventure?");
-        System.out.print(" ");
-        Start = keyboard.next();
-
-        if (Start.equalsIgnoreCase("yes")) {
-            int linesLeft = 40;
-            while (linesLeft > 0) {
-                System.out.println();
-                linesLeft = linesLeft - 1;
-            }
-            System.out.println("    You wake up in a poorly lit room with very little furniture and barred 'windows'. ");
-            System.out.println("In the corner of the room sits a derelict 'chest' that looks like it hasn't been");
-            System.out.println("opened in centuries. On the opposite end of the room rests a rotten oak-wood 'desk'.");
-            System.out.println("What do you inspect first?");
-            System.out.print(" ");
-            Go = keyboard.next();
-
-            if (Go.equalsIgnoreCase("desk")) {
-                System.out.println("You walk over to the desk to see a stack of blank 'papers' on top of it and");
-                System.out.println("and a 'drawer' on it's front.");
-                System.out.print(" ");
-                Drawer = keyboard.next();
-
-                if (Drawer.equalsIgnoreCase("drawer")) {
-                    System.out.println("You open the drawer to find a small onyx key.");
-                    System.out.println("Type return to continue.");
-                    System.out.print(" ");
-                    Start = keyboard.next();
-                }
-            }
-
-            else if (Go.equalsIgnoreCase("chest")) {
-                System.out.println("You walk over to the chest to see a 'lock' on the front of it.");
-                System.out.print(" ");
-            }
-        }
-        else if (Start.equalsIgnoreCase("return"));
-        System.out.println("You are back in the center of the room.");
     }
 }
